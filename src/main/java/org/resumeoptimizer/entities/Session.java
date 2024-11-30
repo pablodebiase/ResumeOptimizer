0fc099543d1e63434426d9f84136749f3de770a5
Hongo <hongo@example.com>
2024-11-29 19:00:17 -0700

    Optimized imports

--- src/main/java/org/resumeoptimizer/entities/Session.java
+++ src/main/java/org/resumeoptimizer/entities/Session.java
@@ -1,9 +1,9 @@
 package org.resumeoptimizer.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/org/resumeoptimizer/entities/User.java
+++ src/main/java/org/resumeoptimizer/entities/User.java
@@ -3,8 +3,8 @@ package org.resumeoptimizer.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
